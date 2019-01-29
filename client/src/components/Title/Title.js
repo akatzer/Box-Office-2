f1b9fbc2073043735fd63b73ea87d9d412958c96
Addison Murry <addymurry@gmail.com>
2019-01-28 19:37:35 -0600

    login

--- client/src/components/Title/Title.js
+++ client/src/components/Title/Title.js
@@ -34,13 +34,13 @@ class Title extends React.Component {
                 <div>
                     <Button className="login" onClick={this.toggle}><i className="fas fa-user"></i>{this.props.buttonLabel}</Button>
                     <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                        <ModalHeader toggle={this.toggle}>Login/Sign Up</ModalHeader>
+                        <ModalHeader toggle={this.toggle}>Sign Up</ModalHeader>
                         <ModalBody>
                         <Input id="searchInput" placeholder="Email" />
                         <Input id="searchInput" placeholder="Password" />
           </ModalBody>
                         <ModalFooter>
-                            <Button color="primary" onClick={this.toggle}>Log In</Button>{' '}
+                            <Button color="primary" onClick={this.toggle}>Sign Up</Button>{' '}
                             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
                         </ModalFooter>
                     </Modal>
